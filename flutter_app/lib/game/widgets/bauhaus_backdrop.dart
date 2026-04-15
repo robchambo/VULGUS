113a28d58ee065fa316d58043ce910f8933d3022
Robchambo <info.autex@gmail.com>
2026-04-15 11:38:49 -0700

    feat(game): BauhausBackdrop decorative shapes

---
+++ flutter_app/lib/game/widgets/bauhaus_backdrop.dart
@@ -0,0 +1,56 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import '../../theme/app_colors.dart';
+
+class BauhausBackdrop extends StatelessWidget {
+  const BauhausBackdrop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IgnorePointer(
+      child: Stack(
+        children: [
+          Positioned(
+            top: 96,
+            left: -40,
+            child: Transform.rotate(
+              angle: pi / 4,
+              child: Container(
+                width: 112,
+                height: 112,
+                color: AppColors.primary.withValues(alpha: 0.05),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 96,
+            right: -80,
+            child: Container(
+              width: 224,
+              height: 224,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: AppColors.secondaryContainer.withValues(alpha: 0.5),
+                  width: 4,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 2 - 40,
+            right: 32,
+            child: Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: AppColors.tertiary.withValues(alpha: 0.1),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
